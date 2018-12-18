f697b5cd65fb5cb2dc9d1585f8ede11b8fe01f50
Schessner <schessner@w2k.biochem.mpg.de>
2018-12-18 10:47:02 +0100

    Test 1 of full script

--- Scripts/FilterTopDistances.R
+++ Scripts/FilterTopDistances.R
@@ -12,11 +12,24 @@ p <- add_argument(p, 'infile', help='Do not touch, comes from Perseus')
 p <- add_argument(p, 'outfile', help='Do not touch, comes from Perseus')
 
 argp <- parse_args(p, argv)
+
 # Read data input
+library(PerseusR)
+data_perseus <- read.perseus(argp$infile)
+data <- main(data_perseus)
+annotations <- annotCols(data_perseus)
+
+# Extract id column
+stopifnot(argp$id %in% names(annotations))
+id <- annotations[[argp$id]]
 
 # Analyse data
-#distances <- pairwise_distances(data)
-#distances_annotated <- annotate_distances(distances, id)
-#result <- filterTopn(distances_annotated, topn)
+library(SpatialDistance)
+distances <- pairwise_distances(data)
+distances_annotated <- annotate_distances(distances, id)
+result <- filterTopn(distances_annotated, argp$topn)
 
 # Write Perseus output
+output <- matrixData(main = result['distance'],
+                     annotCols = result[c('id_1', 'id_2')])
+write.perseus(output, argp$outfile)
