abbc08c4c20c365dadda58f611883da8ccad6911
Schessner <schessner@w2k.biochem.mpg.de>
2018-12-18 10:15:34 +0100

    Function pairwise_distances added and tested

---
+++ R/DistanceFunctions.R
@@ -0,0 +1,10 @@
+#' Pairwise distances between rows
+#' @examples
+#' data <- matrix(c(1,2,3,2,3,2), nrow=3)
+#' pairwise_distances(data)
+pairwise_distances <- function(data) {
+  distances <- as.matrix(dist(data))
+
+  distances[upper.tri(distances)] <- NA
+  diag(distances) <- NA
+}
